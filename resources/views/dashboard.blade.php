bd4dc6b2a3aa38317ac19c7b65ee510d1908b976
ogawa <uncle.sum1961@gmail.com>
2024-07-02 18:26:57 +0900

    http化をやめた

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,19 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+<head>
+    <meta charset="utf-8">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('test.css') }}">
+</head>
+
+<body>
+    <x-app-layout>
+        <x-slot name="header">
+        </x-slot>
+
+        <p>ここにページの内容をいれます</p>
+
+    </x-app-layout>
+</body>
